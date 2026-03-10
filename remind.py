5b5d38dcd64571fa29560f098ffadd7b705550fb
15614112639 <111230160+15614112639@users.noreply.github.com>
2026-03-10 18:28:35 +0800

    Update remind.py

--- remind.py
+++ remind.py
@@ -3,8 +3,9 @@ from wxpusher import WxPusher
 APP_TOKEN = "AT_6xOCcOPd4vfVETlGAvcZ36jcTNCvLx3e"
 TOPIC_ID = 43605
 
-WxPusher.send_message(
+result = WxPusher.send_message(
     content="⏰ 记得打卡！",
     topic_ids=[TOPIC_ID],
     token=APP_TOKEN
 )
+print(result)
