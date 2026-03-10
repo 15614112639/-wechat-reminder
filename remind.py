648d970cecd6367364b19a587c3aeb643b2a0ad3
15614112639 <111230160+15614112639@users.noreply.github.com>
2026-03-10 17:46:34 +0800

    Create remind.py

---
+++ remind.py
@@ -0,0 +1,11 @@
+from wxpusher import WxPusher
+import datetime
+
+APP_TOKEN = "AT_6xOCcOPd4vfVETlGAvcZ36jcTNCvLx3e"
+UID = "UID_Jcp4BjUmmbHPzMjsXaO4gdgjoX8J"
+
+WxPusher.send_message(
+    "⏰ 记得打卡！",
+    uids=[UID],
+    app_token=APP_TOKEN
+)
